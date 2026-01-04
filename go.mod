632533c40df8b4b925db1dc04f9906e6cd54d4e7
Vasyl Stetsuryn <vasyl@vasyl.org>
2026-01-04 15:19:56 +0100

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module trmnl-server-go
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Status         int    `json:"status,omitempty"`
+	ImageURL       string `json:"image_url,omitempty"`
+	Filename       string `json:"filename"`
+	UpdateFirmware bool   `json:"update_firmware"`
+	FirmwareUrl    string `json:"firmware_url"`
+	RefreshRate    int    `json:"refresh_rate"`
+	ResetFirmware  bool   `json:"reset_firmware"`
+}
+
+func renderDisplay() (res []byte) {
+	r := Response{
+		Status:         0,
+		ImageURL:       "http://172.16.30.187:8080/public/london.png",
+		Filename:       "2024-09-20T00:00:00",
+		UpdateFirmware: false,
+		FirmwareUrl:    "",
+		RefreshRate:    30,
+		ResetFirmware:  false,
+	}
+
+	res, err := json.Marshal(r)
+	if err != nil {
+		log.Fatalf("Error occurred during marshalling: %s", err.Error())
+	}
+	return res
+}
+
+func Run(branch, commithash, version, port string) {
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		msg := fmt.Sprintf("App version: %s, commit: %s branch: %s ", version, commithash, branch)
+		w.WriteHeader(200)
+		w.Write([]byte(msg))
+	})
+
+	http.HandleFunc("/public/", ServeFiles)
+
+	http.HandleFunc("/api/display", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Rendering display for device: %s \n", r.Header.Get("ID"))
+		// for k, v := range r.Header {
+		// 	log.Printf("Header field %s, Value %s \n", k, v)
+		// }
+
+		msg := renderDisplay()
+		w.WriteHeader(200)
+		// w.Header.Set("Content-Type", "application/json")
+		w.Write([]byte(msg))
+	})
+	log.Printf("Branch: %s, CommitHash: %s, Version: %s \n", branch, commithash, version)
+	log.Printf("HTTP server started on port %s \n", port)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func ServeFiles(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Requested file %s from %s", r.URL.Path, r.Form)
+	p := "." + r.URL.Path
+	http.ServeFile(w, r, p)
+}
+
+func main() {
+	Run("main", "00000", "0.0.1", "8080")
+}

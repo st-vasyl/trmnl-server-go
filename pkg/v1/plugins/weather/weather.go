fd788669d1e40553d00d6f6db1a0283c84ebd114
Vasyl Stetsuryn <vasyl@vasyl.org>
2026-01-23 22:30:42 +0100

    glamourized weather

--- pkg/v1/plugins/weather/weather.go
+++ pkg/v1/plugins/weather/weather.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"time"
+	"trmnl-server-go/pkg/v1/icons"
 	"trmnl-server-go/pkg/v1/render"
 )
 
@@ -50,6 +51,7 @@ type WeatherUnits struct {
 	Apparent_temperature string `json:"apparent_temperature"`
 	Relative_humidity_2m string `json:"relative_humidity_2m"`
 	Wind_speed_10m       string `json:"wind_speed_10m"`
+	Wind_gusts_10m       string `json:"wind_gusts_10m"`
 	Surface_pressure     string `json:"surface_pressure"`
 	Weather_code         string `json:"weather_code"`
 }
@@ -61,6 +63,7 @@ type WeatherData struct {
 	Apparent_temperature float64 `json:"apparent_temperature"`
 	Relative_humidity_2m int     `json:"relative_humidity_2m"`
 	Wind_speed_10m       float64 `json:"wind_speed_10m"`
+	Wind_gusts_10m       float64 `json:"wind_gusts_10m"`
 	Surface_pressure     float64 `json:"surface_pressure"`
 	Weather_code         int     `json:"weather_code"`
 }
@@ -91,7 +94,7 @@ func getLocation(city string) (Location, error) {
 func getWeather(l Location) (Weather, error) {
 	var w Weather
 
-	url := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&current=temperature_2m,apparent_temperature,relative_humidity_2m,wind_speed_10m,surface_pressure,weather_code&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunset,sunrise&wind_speed_unit=ms&timezone=auto", l.LocationResult[0].Latitude, l.LocationResult[0].Longitude)
+	url := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&current=temperature_2m,apparent_temperature,relative_humidity_2m,wind_speed_10m,wind_gusts_10m,surface_pressure,weather_code&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunset,sunrise&wind_speed_unit=ms&timezone=auto", l.LocationResult[0].Latitude, l.LocationResult[0].Longitude)
 	r, err := http.Get(url)
 	r.Header.Set("Accept", "application/json")
 	r.Header.Set("Accept-Language", "en-US")
@@ -116,6 +119,7 @@ func RenderScreenWeather(width, height int, city, filename string, voltage float
 	l, _ := getLocation(city)
 	w, _ := getWeather(l)
 	img := render.NewImage(width, height)
+	weatherImage := getWeatherConditionByCode(w.Current.Weather_code)
 
 	var temperature render.ChartRecords
 	for i := range len(w.Hourly.Time) {
@@ -126,27 +130,72 @@ func RenderScreenWeather(width, height int, city, filename string, voltage float
 		temperature.ChartRecord = append(temperature.ChartRecord, tmp)
 	}
 
-	if err := render.AddText(img, fmt.Sprintf("%.1f %s", w.Current.Temperature_2m, w.CurrentUnits.Temperature_2m), image.Point{50, 50}, color.Black, 50); err != nil {
+	// Adding Weather conditions icon
+	if err := render.AddImageFromBase64(img, weatherImage, image.Point{-50, 0}); err != nil {
 		return err
 	}
 
-	if err := render.AddText(img, fmt.Sprintf("Feels: %.1f %s", w.Current.Apparent_temperature, w.CurrentUnits.Temperature_2m), image.Point{50, 100}, color.Black, 30); err != nil {
+	// Adding current temperature
+	if err := render.AddText(img, fmt.Sprintf("%.1f%s", w.Current.Temperature_2m, w.CurrentUnits.Temperature_2m), image.Point{30, 170}, color.Black, 50); err != nil {
 		return err
 	}
 
-	if err := render.AddText(img, fmt.Sprintf("Humidity: %d %s", w.Current.Relative_humidity_2m, w.CurrentUnits.Relative_humidity_2m), image.Point{50, 150}, color.Black, 30); err != nil {
+	// Adding feels like temperature icon
+	if err := render.AddImageFromBase64(img, icons.Temperature, image.Point{-293, -20}); err != nil {
 		return err
 	}
 
-	if err := render.AddText(img, fmt.Sprintf("Wind: %.1f m/s", w.Current.Wind_speed_10m), image.Point{400, 50}, color.Black, 30); err != nil {
+	if err := render.AddText(img, fmt.Sprintf("%.1f%s", w.Current.Apparent_temperature, w.CurrentUnits.Temperature_2m), image.Point{360, 50}, color.Black, 30); err != nil {
 		return err
 	}
 
-	if err := render.AddText(img, fmt.Sprintf("Min: %.1f %s", w.Daily.TMin[0], w.CurrentUnits.Temperature_2m), image.Point{400, 100}, color.Black, 30); err != nil {
+	// Adding max temperature icon
+	if err := render.AddImageFromBase64(img, icons.TemperatureHigh, image.Point{-300, -70}); err != nil {
 		return err
 	}
 
-	if err := render.AddText(img, fmt.Sprintf("Max: %.1f %s", w.Daily.TMax[0], w.CurrentUnits.Temperature_2m), image.Point{400, 150}, color.Black, 30); err != nil {
+	// Adding max temperature
+	if err := render.AddText(img, fmt.Sprintf("%.1f%s", w.Daily.TMax[0], w.CurrentUnits.Temperature_2m), image.Point{360, 100}, color.Black, 30); err != nil {
+		return err
+	}
+
+	// Adding min temperature icon
+	if err := render.AddImageFromBase64(img, icons.TemperatureLow, image.Point{-300, -120}); err != nil {
+		return err
+	}
+
+	// Adding min temperature
+	if err := render.AddText(img, fmt.Sprintf("%.1f%s", w.Daily.TMin[0], w.CurrentUnits.Temperature_2m), image.Point{360, 150}, color.Black, 30); err != nil {
+		return err
+	}
+
+	// Add humidity icon
+	if err := render.AddImageFromBase64(img, getHumidity(w.Current.Relative_humidity_2m), image.Point{-530, -20}); err != nil {
+		return err
+	}
+
+	// Add humidity
+	if err := render.AddText(img, fmt.Sprintf("%d%s", w.Current.Relative_humidity_2m, w.CurrentUnits.Relative_humidity_2m), image.Point{590, 50}, color.Black, 30); err != nil {
+		return err
+	}
+
+	// Add Wind icon
+	if err := render.AddImageFromBase64(img, icons.Wind, image.Point{-530, -70}); err != nil {
+		return err
+	}
+
+	// Add Wind
+	if err := render.AddText(img, fmt.Sprintf("%.1fm/s", w.Current.Wind_speed_10m), image.Point{590, 100}, color.Black, 30); err != nil {
+		return err
+	}
+
+	// Add Wind Gusts icon
+	if err := render.AddImageFromBase64(img, icons.WindGusts, image.Point{-530, -120}); err != nil {
+		return err
+	}
+
+	// Add Wind Gusts
+	if err := render.AddText(img, fmt.Sprintf("%.1fm/s", w.Current.Wind_gusts_10m), image.Point{590, 150}, color.Black, 30); err != nil {
 		return err
 	}
 
@@ -159,4 +208,48 @@ func RenderScreenWeather(width, height int, city, filename string, voltage float
 	}
 
 	return nil
+}
+
+func getWeatherConditionByCode(weatherCode int) string {
+	var weatherImage string
+
+	switch {
+	case weatherCode == 0:
+		weatherImage = icons.WeatherCode0
+	case weatherCode == 1:
+		weatherImage = icons.WeatherCode1
+	case weatherCode == 2:
+		weatherImage = icons.WeatherCode1
+	case weatherCode == 3:
+		weatherImage = icons.WeatherCode3
+	case weatherCode == 45 || weatherCode == 48:
+		weatherImage = icons.WeatherCode4
+	case weatherCode > 50 && weatherCode < 70:
+		weatherImage = icons.WeatherCode5
+	case weatherCode > 70 && weatherCode < 76:
+		weatherImage = icons.WeatherCode7
+	case weatherCode == 77:
+		weatherImage = icons.WeatherCode77
+	case weatherCode == 80 || weatherCode == 81 || weatherCode == 82:
+		weatherImage = icons.WeatherCode8
+	case weatherCode == 85 || weatherCode == 86:
+		weatherImage = icons.WeatherCode85
+	case weatherCode > 90:
+		weatherImage = icons.WeatherCode9
+	}
+	return weatherImage
+}
+
+func getHumidity(humidity int) string {
+	var humidityImage string
+
+	switch {
+	case humidity < 50:
+		humidityImage = icons.HumidityLow
+	case humidity >= 50 && humidity < 80:
+		humidityImage = icons.HumidityMid
+	case humidity >= 80:
+		humidityImage = icons.HumidityHigh
+	}
+	return humidityImage
 }
